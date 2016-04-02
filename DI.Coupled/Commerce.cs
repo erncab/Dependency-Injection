7fd2abfbb892635e1961b0b1482da18de6831c78
erncab@yahoo.com <exitos236942>
2016-04-02 15:22:25 -0400

    Renamed attributes.

--- DI.Coupled/Commerce.cs
+++ DI.Coupled/Commerce.cs
@@ -6,25 +6,30 @@ namespace DI.Coupled
     {
         public Commerce()
         {
-            _BillingProcessor = new BillingProcessor();
-            _Customer = new Customer();
-            _Notifier = new Notifier();
-            _Logger = new Logger();
+            _billingProcessor = new BillingProcessor();
+            _customer = new Customer();
+            _notifier = new Notifier();
+            _logger = new Logger();
         }
-
-        readonly BillingProcessor _BillingProcessor;
-        readonly Customer _Customer;
-        readonly Notifier _Notifier;
-        readonly Logger _Logger;
 
         public void ProcessOrder(OrderInfo orderInfo)
         {
-            _BillingProcessor.ProcessPayment(orderInfo.CustomerName, orderInfo.CreditCard, orderInfo.Price);
-            _Logger.Log("Billing processed");
-            _Customer.UpdateCustomerOrder(orderInfo.CustomerName, orderInfo.Product);
-            _Logger.Log("Customer updated");
-            _Notifier.SendReceipt(orderInfo);
-            _Logger.Log("Receipt sent");
+            _billingProcessor.ProcessPayment(orderInfo.CustomerName, orderInfo.CreditCard, orderInfo.Price);
+
+            _logger.Log("Billing processed");
+
+            _customer.UpdateCustomerOrder(orderInfo.CustomerName, orderInfo.Product);
+
+            _logger.Log("Customer updated");
+
+            _notifier.SendReceipt(orderInfo);
+
+            _logger.Log("Receipt sent");
         }
+
+        readonly BillingProcessor _billingProcessor;
+        readonly Customer _customer;
+        readonly Notifier _notifier;
+        readonly Logger _logger;
     }
 }
